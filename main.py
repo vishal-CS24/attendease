7cca6e4c0995a358441555de34587eec6098f9d9
vishal-cs24 <mahajanvishu722@gmail.com>
2023-10-09 23:20:24 +0530

    background modified

--- main.py
+++ main.py
@@ -10,41 +10,45 @@ from attendance import Attendance_mgmnt
 class Attendease():
     def __init__(self,root):
          self.root= root
-         self.root.geometry("1530x790+0+0")
+         self.root.geometry("1535x835+0+0")
          self.root.title("ATTENDEASE")
          self.root.iconbitmap("./attendese\pictures\icon.ico")
 #-----------------------Top bar--------------------------------------------------------------
-        #  #first image
-         img=Image.open(r"attendese\pictures\h4.jpeg")
-         img=img.resize((550,130),Image.ANTIALIAS)
-         self.photoimg=ImageTk.PhotoImage(img)
-         f_lbl=Label(self.root,image=self.photoimg)
-         f_lbl.place(x=0,y=0,width=550,height=130)
+     #    #  #first image
+     #     img=Image.open(r"attendese\pictures\h4.jpeg")
+     #     img=img.resize((550,130),Image.ANTIALIAS)
+     #     self.photoimg=ImageTk.PhotoImage(img)
+     #     f_lbl=Label(self.root,image=self.photoimg)
+     #     f_lbl.place(x=0,y=0,width=550,height=130)
 
-        #  #2nd image
-         img1=Image.open(r"attendese\pictures\h4.jpeg")
-         img1=img1.resize((500,130),Image.ANTIALIAS)
-         self.photoimg1=ImageTk.PhotoImage(img1)
-         f_lbl1=Label(self.root,image=self.photoimg1)
-         f_lbl1.place(x=550,y=0,width=500,height=130)
+     #    #  #2nd image
+     #     img1=Image.open(r"attendese\pictures\h4.jpeg")
+     #     img1=img1.resize((500,130),Image.ANTIALIAS)
+     #     self.photoimg1=ImageTk.PhotoImage(img1)
+     #     f_lbl1=Label(self.root,image=self.photoimg1)
+     #     f_lbl1.place(x=550,y=0,width=500,height=130)
 
-         #3rd image
-         img2=Image.open(r"attendese\pictures\h4.jpeg")
-         img2=img2.resize((550,130),Image.ANTIALIAS)
-         self.photoimg2=ImageTk.PhotoImage(img2)
-         f_lbl2=Label(self.root,image=self.photoimg2)
-         f_lbl2.place(x=1050,y=0,width=550,height=130)
+     #     #3rd image
+     #     img2=Image.open(r"attendese\pictures\h4.jpeg")
+     #     img2=img2.resize((550,130),Image.ANTIALIAS)
+     #     self.photoimg2=ImageTk.PhotoImage(img2)
+     #     f_lbl2=Label(self.root,image=self.photoimg2)
+     #     f_lbl2.place(x=1050,y=0,width=550,height=130)
         # ----------------------------------------#background image---------------------------------------------------------------
-         bgimg=Image.open(r"attendese\pictures\h18.jpg")
-         bgimg=bgimg.resize((1530,790),Image.ANTIALIAS)
-         self.bgphotoimg=ImageTk.PhotoImage(bgimg)
-         bglbl=Label(self.root,image=self.bgphotoimg)
-         bglbl.place(x=0,y=130,width=1530,height=790)
+     #     bgimg=Image.open(r"attendese\pictures\loginback1.jpg")
+     #     bgimg=bgimg.resize((1530,790),Image.ANTIALIAS)
+     #     self.bgphotoimg=ImageTk.PhotoImage(bgimg)
+     #     bglbl=Label(self.root,image=self.bgphotoimg)
+     #     bglbl.place(x=0,y=0,width=1530,height=790)
+
+         self.bg=ImageTk.PhotoImage(file="./attendese\pictures\loginback1.jpg")
+         bglbl=Label(self.root,image=self.bg)
+         bglbl.place(x=0,y=0,relwidth=1,relheight=1)
          #------------------------------------------Title--------------------------------------------------------------------
-         title=Label(bglbl,text="ATTENDEASE",font=("Rockwell Condensed",25,"bold"),bg="#ace5ee",fg="red")
-         title.place(x=0,y=0,width=1530,height=45)
-         title1=Label(bglbl,text="(A Machine learning based Attendance System)",font=("Arial", 15,"bold"),bg="#ace5ee",fg="red")
-         title1.place(x=0,y=40,width=1530,height=45)
+     #     title=Label(bglbl,text="ATTENDEASE",font=("Rockwell Condensed",25,"bold"),bg="#ace5ee",fg="red")
+     #     title.place(x=0,y=0,width=1530,height=45)
+     #     title1=Label(bglbl,text="(A Machine learning based Attendance System)",font=("Arial", 15,"bold"),bg="#ace5ee",fg="red")
+     #     title1.place(x=0,y=40,width=1530,height=45)
          #--------------------Student Button--------------------------
          stdbutton=Image.open(r"attendese\pictures\st.jpg")
          stdbutton=stdbutton.resize((220,220),Image.ANTIALIAS)
