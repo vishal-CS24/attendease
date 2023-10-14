4227d5d0cf7f46907ae0b4f63dcb2a06916d6c69
vishal-cs24 <mahajanvishu722@gmail.com>
2023-10-15 00:38:50 +0530

    stored data in database

--- register.py
+++ register.py
@@ -3,6 +3,7 @@ from tkinter import ttk, messagebox,filedialog
 from PIL import Image,ImageTk
 import customtkinter as ct
 import mysql.connector
+import re
 class register_Page():
     def __init__(self,root):
          self.root= root
@@ -24,24 +25,24 @@ class register_Page():
          wbg_lbl.place(x=0,y=0,relwidth=1,relheight=1)
 
          #============Entry fields=========
-         self.name = ct.CTkEntry(wbg_lbl,placeholder_text="Enter your Fullname",width=300,height=50,border_width=2,font=("times new roman",20),fg_color="#1d1325",bg_color="#1d1325",textvariable=self.var_fname)
+         self.name = ct.CTkEntry(wbg_lbl,width=300,height=50,border_width=2,font=("times new roman",20),fg_color="#1d1325",bg_color="#1d1325",textvariable=self.var_fname)
          self.name.place(x=50,y=100)
         #========password================
-         self.contactNumber = ct.CTkEntry(wbg_lbl,textvariable=self.var_contact, placeholder_text="Enter your contact number",width=300,height=50,font=("times new roman",20),fg_color="#1d1325",bg_color="#1d1325")
+         self.contactNumber = ct.CTkEntry(wbg_lbl,textvariable=self.var_contact,width=300,height=50,font=("times new roman",20),fg_color="#1d1325",bg_color="#1d1325")
          self.contactNumber.place(x=400,y=100)
 
-         self.emailid = ct.CTkEntry(wbg_lbl,textvariable=self.var_mail, placeholder_text="Enter your email id",width=300,height=50,border_width=2,font=("times new roman",20),fg_color="#1d1325",bg_color="#1d1325")
+         self.emailid = ct.CTkEntry(wbg_lbl,textvariable=self.var_mail, width=300,height=50,border_width=2,font=("times new roman",20),fg_color="#1d1325",bg_color="#1d1325")
          self.emailid.place(x=50,y=175)
         #========password================
-         self.login_password = ct.CTkEntry(wbg_lbl,placeholder_text="Enter your Password",textvariable=self.var_password, width=300,height=50,border_width=2,font=("times new roman",20),bg_color="#1d1325",fg_color="#1d1325")
+         self.login_password = ct.CTkEntry(wbg_lbl,textvariable=self.var_password ,width=300,height=50,border_width=2,font=("times new roman",20),bg_color="#1d1325",fg_color="#1d1325")
          self.login_password.place(x=400,y=175)
 
-        #  self.question = ct.CTkEntry(wbg_lbl, placeholder_text="Enter your Question",width=300,height=35,font=("times new roman",20))
+        #  self.question = ct.CTkEntry(wbg_lbl, ",width=300,height=35,font=("times new roman",20))
          self.question=ct.CTkComboBox(wbg_lbl,variable=self.var_question,state="readonly",values=["Select Security Question","Your Birth Place","Your latest school","Favourite Pet"],width=300,height=50,bg_color="#1d1325",fg_color="#1d1325",font=("times new roman",20),button_hover_color="#140532",dropdown_font=("times new roman",20))
          self.question.set("Select Security Question")
          self.question.place(x=50,y=250)
         #========password================
-         self.answer = ct.CTkEntry(wbg_lbl,textvariable=self.var_answer,bg_color="#1d1325",fg_color="#1d1325", placeholder_text="Enter your Answer",width=300,height=50,font=("times new roman",20))
+         self.answer = ct.CTkEntry(wbg_lbl,textvariable=self.var_answer,bg_color="#1d1325",fg_color="#1d1325", width=300,height=50,font=("times new roman",20))
          self.answer.place(x=400,y=250)
          #======================check box==================
          self.chkbox=ct.CTkCheckBox(wbg_lbl,variable=self.var_check,text="I Agree the Terms and Conditions",hover_color="#210953",width=650,height=50,fg_color="#1d1325",bg_color="#1d1325",border_width=2,corner_radius=5,onvalue=1,offvalue=0)
@@ -54,10 +55,34 @@ class register_Page():
          reg_btn.place(x=50,y=400)
 #=========function declaration=============
     def register_Data(self):
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
         if self.var_fname.get()=="Full Name" or self.var_mail.get()=="Email" or self.var_password.get()=="Password" or self.var_question.get()=="Select Security Question" or self.var_contact.get()=="Contact Number" or self.var_answer.get()=="Security Answer":
             messagebox.showwarning("Required","All fields are Required")
-        else:
+        elif len(self.var_contact.get())!=10:
+          messagebox.showwarning("Phone number Error","Please add 10 digit phone number",parent=self.root)
+        elif not(re.fullmatch(regex, self.var_mail.get())):
+          messagebox.showwarning("Email Error","Please add Valid email Address",parent=self.root)
+        elif self.var_check.get()!="1":
+          messagebox.showwarning("Check Box error","Please Accept Terms and COnditions",parent=self.root)
 
+        else:
+            try:
+                conn=mysql.connector.connect(host="localhost",user="root",password="root",database="attendease_db")
+                my_cursor=conn.cursor()      
+                my_cursor.execute("insert into registerdb values(%s,%s,%s,%s,%s,%s)",(
+                  self.var_fname.get(),
+                  self.var_contact.get(),
+                  self.var_mail.get(),
+                  self.var_question.get(),
+                  self.var_answer.get(),
+                  self.var_password.get()
+               ))
+                conn.commit()
+                conn.close()
+                messagebox.showinfo("sucess","Student Details has been added Sucessfully!",parent=self.root)
+            except Exception as es:
+               messagebox.showerror("Error",f"Due to :{str(es)}",parent=self.root)
+            
 
 if __name__=="__main__":
     root=Tk()
