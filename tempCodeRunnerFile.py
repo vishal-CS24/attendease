2cd15d2167bbc1936d24594d2625841de1eebcb2
vishal-cs24 <mahajanvishu722@gmail.com>
2023-10-21 00:01:22 +0530

    add attendance file

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-ec850a
+
+         lbgimg=Image.open("./attendese\pictures\ATTENDEASE.png")
+         lbgimg=lbgimg.resize((565,380),Image.ANTIALIAS)
+         self.bglimg1=ImageTk.PhotoImage(lbgimg)
+         lbg_lbl=Label(frame,image=self.bglimg1)
+         lbg_lbl.place(x=0,y=0,width=565,height=380)
