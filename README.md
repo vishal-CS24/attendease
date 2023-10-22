48584f3b51a47a0df8b2d51982df698c73a71cea
vishal-cs24 <mahajanvishu722@gmail.com>
2023-10-23 00:12:13 +0530

    modify readme file

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-It is a machine learning based attendance system 
+# Face Recognisation based Attendance System using LBPH Algorithm
+
+![Attendance System Logo](./pictures/homepage.png)
+
+## Overview
+
+Welcome to the Attendance System using Machine Learning with the LBPH (Local Binary Pattern Histogram) algorithm. This project empowers teachers to streamline the process of managing student attendance using the power of computer vision and machine learning. With this system, you can effortlessly:
+
+- Enter student details in the student section.
+- Capture student photos through your camera.
+- Train the LBPH model for facial recognition.
+- Take attendance using your webcam and automatically record it in an Excel file.
+
+## Features
+
+- **User-Friendly Interface**: Designed with teachers in mind for ease of use.
+- **Student Management**: Conveniently add, update, or delete student information.
+- **Camera Integration**: Capture student images directly from your webcam.
+- **LBPH Algorithm**: Utilize Local Binary Pattern Histogram for accurate facial recognition.
+- **Attendance Recording**: Automatically save attendance data in an Excel file.
+- **Model Training**: Train the model to improve recognition accuracy.
+
--- forgot.py
+++ forgot.py
@@ -1,11 +1,7 @@
 from tkinter import *
 from tkinter import ttk, messagebox,filedialog
-from PIL import Image,ImageTk
 import customtkinter as ct
-from register import register_Page
-from main import Attendease
 import mysql.connector
-
 class forgot_passwd():
     def __init__(self,root):
          self.root= root
@@ -14,25 +10,19 @@ class forgot_passwd():
          self.root.iconbitmap("./attendese\pictures\icon.ico")
          self.root.configure(bg="#1d1325")
          self.root.maxsize(400,300)
-
          self.var_question=StringVar()
          self.var_answer=StringVar(value="Security Answer")
          self.var_mail=StringVar(value="Email")
-
-          #  self.question = ct.CTkEntry(wbg_lbl, ",width=300,height=35,font=("times new roman",20))
          self.question=ct.CTkComboBox(self.root,variable=self.var_question,state="readonly",values=["Select Security Question","Your Birth Place","Your latest school","Favourite Pet"],width=300,height=50,bg_color="#1d1325",fg_color="#1d1325",font=("times new roman",20),button_hover_color="#140532",dropdown_font=("times new roman",20))
          self.question.set("Select Security Question")
          self.question.place(x=50,y=20)
         #========password================
          self.answer = ct.CTkEntry(self.root,textvariable=self.var_answer,bg_color="#1d1325",fg_color="#1d1325", width=300,height=50,font=("times new roman",20))
          self.answer.place(x=50,y=80)
-
          self.emailid = ct.CTkEntry(self.root,textvariable=self.var_mail, width=300,height=50,border_width=2,font=("times new roman",20),fg_color="#1d1325",bg_color="#1d1325")
          self.emailid.place(x=50,y=140)
-
          self.fetch_button=ct.CTkButton(self.root,text="Get password",command=self.fetch_Password,width=300,height=50,cursor="hand2",fg_color="#34675f",hover_color="#1f2435",bg_color="#1d1325")
          self.fetch_button.place(x=50,y=200)
-
     def fetch_Password(self):
         if self.var_mail.get()=="Email" or self.var_question.get()=="Select Security Question" or self.var_answer.get()=="Security Answer":
             messagebox.showwarning("Required","All fields are Required",parent=self.root)
@@ -47,7 +37,6 @@ class forgot_passwd():
                 messagebox.showinfo("Forgot Password",f"Your password is {data[0][0]}",parent=self.root)
             except Exception as es:
                 messagebox.showerror("Error",f"Due to :{str(es)}",parent=self.root)
-
 if __name__=="__main__":
     root=Tk()
     obj=forgot_passwd(root)
